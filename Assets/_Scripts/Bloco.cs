bba82c3358bab4925bba18d2225c4ad070ffad46
Bilbia <biamcs2000@gmail.com>
2022-02-21 21:46:01 -0300

    sprites que mudam com durabilidade

--- Assets/_Scripts/Bloco.cs
+++ Assets/_Scripts/Bloco.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class Bloco : MonoBehaviour
 {
     private int health;
+    private SpriteRenderer render;
+    public Sprite health0;
+    public Sprite health1;
+    public Sprite health2;
+    public Sprite health3;
+    public Sprite health4;
+
+
 
     void Start()
     {
-        health = Random.Range(0, 3);
+        health = Random.Range(0, 4);
+        render = gameObject.GetComponent<SpriteRenderer>();
+        if(health == 0) render.sprite = health0;
+        if(health == 1) render.sprite = health1;
+        if(health == 2) render.sprite = health2;
+        if(health == 3) render.sprite = health3;
+        if(health == 4) render.sprite = health4;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(health == 0) render.sprite = health0;
+        if(health == 1) render.sprite = health1;
+        if(health == 2) render.sprite = health2;
+        if(health == 3) render.sprite = health3;
+        if(health == 4) render.sprite = health4;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
